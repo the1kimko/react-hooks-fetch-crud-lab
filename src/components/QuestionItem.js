183df65cef800bdf7a83882a8cbc2e63d901e4c8
KomfortKimko <komfortkimk@gmail.com>
2024-08-07 10:59:51 +0300

    Fetch-crud-lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onUpdateQuestion, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => onDeleteQuestion(id));
+  }
+
+  function handleCorrectAnswerChange(event) {
+    const updatedQuestion = {
+      ...question,
+      correctIndex: parseInt(event.target.value, 10),
+    };
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex: updatedQuestion.correctIndex }),
+    })
+      .then((r) => r.json())
+      .then(() => onUpdateQuestion(updatedQuestion));
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +39,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
